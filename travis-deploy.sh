6a82d336e0452da5848a654ac6c3c5d48727a2d0
Peter J Slack <pslack@pjslack.com>
2020-02-13 23:54:40 -0500

    fix the zip so it includes parent directory

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -7,9 +7,9 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   echo "OSX DEPLOY PACKAGER"
   try cd "${CODE_DIR}"
   #package up our build
-  try ditto -ck --rsrc --sequesterRsrc "${CODE_DIR}/Builds/MacOSX/build/${BUILD_TYPE}/OMSPD.app" "${CODE_DIR}/build/OMSPD.app.zip"
-  try ditto -ck --rsrc --sequesterRsrc "${CODE_DIR}/Builds/MacOSX/build/${BUILD_TYPE}/OMSPD.component" "${CODE_DIR}/build/OMSPD.component.zip"
-  try ditto -ck --rsrc --sequesterRsrc "${CODE_DIR}/Builds/MacOSX/build/${BUILD_TYPE}/OMSPD.vst3" "${CODE_DIR}/build/OMSPD.vst3.zip"
+  try ditto -ck --rsrc --sequesterRsrc --keepParent "${CODE_DIR}/Builds/MacOSX/build/${BUILD_TYPE}/OMSPD.app" "${CODE_DIR}/build/OMSPD.app.zip"
+  try ditto -ck --rsrc --sequesterRsrc --keepParent "${CODE_DIR}/Builds/MacOSX/build/${BUILD_TYPE}/OMSPD.component" "${CODE_DIR}/build/OMSPD.component.zip"
+  try ditto -ck --rsrc --sequesterRsrc --keepParent "${CODE_DIR}/Builds/MacOSX/build/${BUILD_TYPE}/OMSPD.vst3" "${CODE_DIR}/build/OMSPD.vst3.zip"
 
 fi
 env
